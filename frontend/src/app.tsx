2fdf83dc87532359b84dab61e06ce0095ed25014
Petro Mozil <mozil.petryk@gmail.com>
2023-03-05 13:02:39 +0200

    Add navbar

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,32 +1,26 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
+import { FaHome, FaPen, FaBook } from "react-icons/fa";
+import './index.css'
 import './app.css'
 
-export function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+export function App() {
+    return (
+        <>
+            <div class="main">
+                <nav class="navbar">
+                    <a href="/" class="icon home">
+                        <FaHome />
+                    </a>
+                    <div class="utils">
+                        <a href="/new" class="icon">
+                            <FaPen />
+                        </a>
+                        <a href="/projects" class="icon">
+                            <FaBook />
+                        </a>
+                    </div>
+                </nav>
+            </div>
+        </>
+    )
 }
