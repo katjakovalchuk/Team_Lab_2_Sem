6d3ab7d2048c883444f0dd4c30d10c583a665fff
Petro Mozil <mozil.petryk@gmail.com>
2023-03-10 08:12:25 +0200

    Update Edothr Page

--- frontend/src/components/editPage.tsx
+++ frontend/src/components/editPage.tsx
@@ -1,8 +1,37 @@
 import styles from "../scss/editor.module.scss";
 import TextInput from "./textInput";
 import useScript from "./useScript";
+import { useEffect, useState } from "react";
 
-export default function Editor(props) {
+interface EditorProps {
+    presentationId: string;
+
+};
+
+interface JSONSlide {
+    type: string;
+    content: string;
+}
+
+function ToComponent(obj: JSONSlide) {
+    // switch(obj.type) {
+    //     case "markdown":
+    //     case "text":
+    // }
+    return <></>
+}
+
+export default function Editor(props: EditorProps) {
+    const [slides, setSlides] = useState([]);
+    const [sideIdx, setSlideIdx] = useState([0]);
+
+    useEffect(() => {
+        const headers = { "Content-type": "application/json" };
+        fetch(`/api/presentations/${props.presentationId}`, { headers })
+            .then(resp => resp.json())
+            .then(data => setSlides(data["slides"]))
+    }, [])
+
     return (
         <>
             <div className={styles.sideVew}>
