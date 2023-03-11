793059cd32c31df6d1f4658cdeff2492b5cd5680
Petro Mozil <mozil.petryk@gmail.com>
2023-03-11 19:50:14 +0200

    Remove reveal from frontend

--- frontend/src/components/editPage.js
+++ frontend/src/components/editPage.js
@@ -1,49 +1,19 @@
 import styles from "../scss/editor.module.scss";
-import TextInput from "./textInput";
 import { useEffect, useState } from "react";
-
-function ToSection(obj) {
-    switch (obj.type) {
-        case "other":
-        case "text":
-            return obj.content
-
-        case "code":
-            return (
-                <pre data-id="code">
-                    <code>
-                        {obj.content}
-                    </code>
-                </pre>
-            )
-
-        case "img":
-            return <img src={obj.content} />
-
-        case "iframe":
-            return <iframe allowFullScreen src={obj.content} />
-
-    }
-    return "# Empty slide"
-}
 
 export default function Editor(props) {
     const [slides, setSlides] = useState(
         [
             {
                 content: {
-                    page_one: [
-                        {
-                            type: "text",
-                            content: "# Hiya there! Meesa an empty slide!",
-                            data_transition: "",
-                            bg: "#1f2a31"
-                        }
-                    ]
+                    page_one: {
+                        type: "img",
+                        content: "https://picsum.photos/200/300",
+                    }
                 },
                 dataTransition: "",
                 isMarkdown: true,
-                bg: "1f2a31"
+                bg: "#fff"
             }
         ]
     );
@@ -55,38 +25,12 @@ export default function Editor(props) {
         //     .then(resp => resp.json())
         //     .then(data => setSlides(data["slides"]))
 
-        const clientSideInitialization = async () => {
-            // load modules in browser
-            const Reveal = await (await import("reveal.js")).default
-            const Markdown = await (await import("reveal.js/plugin/markdown/markdown.esm")).default
-            const deck = new Reveal({
-                plugins: [Markdown]
-            })
-            deck.initialize()
-        }
-        clientSideInitialization()
     }, [])
 
     return (
         <>
-            <div className={styles.sideVew}>
-                <div className={styles.editorBox}>
+            <div className={styles.editorBox}>
 
-                </div>
-
-                <div className="reveal">
-                    <div className="slides">
-                        {
-                            slides[slideIdx].isMarkdown ?
-                                (<section data-markdown data-background-color={slides[slideIdx].bg} data-transition={slides[slideIdx].dataTransition}>
-                                    {Object.entries(slides[slideIdx].content).map(v => { return ToSection(v[1]) })}
-                                </section>) :
-                                (<section data-background-color={slides[slideIdx].bg} data-transition={slides[slideIdx].dataTransition}>
-                                    {Object.entries(slides[slideIdx].content).map(v => { return ToSection(v[1]) })}
-                                </section>)
-                        }
-                    </div>
-                </div>
             </div>
         </>
     )
