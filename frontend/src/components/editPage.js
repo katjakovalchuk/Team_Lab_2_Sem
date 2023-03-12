ac3260b3ffb7a6dac69cba029cb49cf0edd15aa4
Petro Mozil <mozil.petryk@gmail.com>
2023-03-12 15:38:28 +0200

    Update slide editor

--- frontend/src/components/editPage.js
+++ frontend/src/components/editPage.js
@@ -1,6 +1,7 @@
 import styles from "../scss/editor.module.scss";
 import ElementEditor from "./editElement";
 import { useEffect, useState } from "react";
+import { FaArrowRight, FaArrowLeft, FaPlus } from "react-icons/fa"
 
 export default function Editor(props) {
     const [slides, setSlides] = useState(
@@ -19,6 +20,21 @@ export default function Editor(props) {
                         content: "https://picsum.photos/200/300",
                     }
                 ],
+            },
+            {
+                name: "slide_2",
+                content: [
+                    {
+                        name: "page_one_2",
+                        type: "img",
+                        content: "https://picsum.photos/200/300",
+                    },
+                    {
+                        name: "page_two_2",
+                        type: "img",
+                        content: "https://picsum.photos/200/300",
+                    }
+                ],
             }
         ]
     );
@@ -28,8 +44,7 @@ export default function Editor(props) {
         let curSlides = slides;
         const updateFn = (val) => {
             curSlides[slideIdx].content[element].content = val;
-            console.log(val);
-            setSlides({ ...curSlides });
+            setSlides(curSlides);
         }
         return updateFn;
     }
@@ -38,8 +53,7 @@ export default function Editor(props) {
         let curSlides = slides;
         const updateFn = (val) => {
             curSlides[slideIdx].content[element].type = val;
-            console.log(val);
-            setSlides({ ...curSlides });
+            setSlides(curSlides);
         }
         return updateFn;
     }
@@ -48,7 +62,7 @@ export default function Editor(props) {
         let curSlides = slides;
         const updateFn = (val) => {
             curSlides[slideIdx].content[element].name = val;
-            setSlides({ ...curSlides });
+            setSlides(curSlides);
         }
         return updateFn;
     }
@@ -58,15 +72,59 @@ export default function Editor(props) {
         // fetch(`/api/presentations/${props.presentationId}`, { headers })
         //     .then(resp => resp.json())
         //     .then(data => setSlides(data["slides"]))
-
     }, [])
 
     return (
         <>
-            <div className={styles.vertical} id="slide_editor">
+            <div className={styles.vertical}>
+                <div className={styles.arrows}>
+                    <FaArrowLeft onClick={() => {
+                        if (slideIdx - 1 >= 0)
+                            setSlideIdx(slideIdx - 1);
+                    }
+                    } />
+                    <FaArrowRight onClick={() => {
+                        if ((slideIdx + 1) == slides.length) {
+                            let curSlides = slides;
+                            curSlides.push(
+                                {
+                                    name: `Slide ${curSlides.length + 1}`,
+                                    content: [
+                                        {
+                                            name: `page_one_${curSlides.length + 1}`,
+                                            type: "img",
+                                            content: "https://picsum.photos/200/300",
+                                        },
+                                        {
+                                            name: "page_two_2",
+                                            type: "img",
+                                            content: "https://picsum.photos/200/300",
+                                        }
+                                    ],
+                                }
+                            )
+                            setSlides(curSlides);
+                        }
+                        setSlideIdx((slideIdx + 1) % slides.length)
+                    }
+                    } />
+                    <FaPlus onClick={() => {
+                        let curSlides = slides;
+                        curSlides[slideIdx].content.push(
+                            {
+                                name: `New component`,
+                                type: "img",
+                                content: "https://picsum.photos/200/300",
+                            }
+                        )
+                        setSlides([...curSlides]);
+                        setSlideIdx(slideIdx);
+                    }} />
+                </div>
+                <h2>{slides[slideIdx].name}</h2>
                 {
                     Object.entries(slides[slideIdx].content).map(
-                        v => <ElementEditor key={v[1].name} type={v[1].type} id={v[1].name} name={v[1].name} required={true} value={v[1].content} updateContent={updateElementContent(v[0])} updateName={updateElementName(v[0])} updateType={updateElementType(v[0])} />
+                        v => <ElementEditor key={`${v[1].name}_${v[0]}`} type={v[1].type} id={v[1].name} name={v[1].name} required={true} value={v[1].content} updateContent={updateElementContent(v[0])} updateName={updateElementName(v[0])} updateType={updateElementType(v[0])} />
                     )
                 }
             </div>
