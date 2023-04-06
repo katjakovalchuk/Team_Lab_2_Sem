eb5069ba2742600fe9a8f70680e61e5338ed6817
radomyr <h.radomyr@proton.me>
2023-04-06 16:57:35 +0300

    fix: try to fix owner column in database

--- src/api/database.py
+++ src/api/database.py
@@ -29,7 +29,6 @@ class SlideObject_db(Base):
     content = sa.Column(String, nullable=True)
     attributes = sa.Column(String, nullable=True)
     object_name = sa.Column(sa.String, primary_key=True)
-    owner = sa.Column(Integer, sa.ForeignKey("slide.slide_id"))
 
     @property
     @contextmanager
@@ -53,8 +52,7 @@ class Slide_db(Base):
     attributes = sa.Column(String, nullable=True)
     background = sa.Column(String, nullable=False)
     max_id = sa.Column(Integer, nullable=True)
-    content = relationship(SlideObject_db)
-    owner = sa.Column(sa.String, sa.ForeignKey("presentation.presentation_name"))
+    content = relationship(SlideObject_db, backref="owner")
 
     @property
     @contextmanager
@@ -82,7 +80,7 @@ class Presentation_db(Base):
     presentation_name = sa.Column(sa.String, primary_key=True)
     style = sa.Column(String, nullable=False)
     plugins = sa.Column(sa.ARRAY(String), nullable=False)
-    slides = relationship(Slide_db)
+    slides = relationship(Slide_db, backref="owner")
     unused_id_max = sa.Column(Integer, nullable=False)
 
     @property
