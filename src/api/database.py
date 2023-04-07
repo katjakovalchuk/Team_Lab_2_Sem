b40b5e22f574176d547d48d50f717e4fb5358806
radomyr <h.radomyr@proton.me>
2023-04-07 18:29:54 +0300

    temp: try2005

--- src/api/database.py
+++ src/api/database.py
@@ -108,6 +108,7 @@ class Presentation_db(Base):
             self.slides.append(slide_db)
         self.owner = presentation.owner
         self.presentation_name = presentation.name
+        self.commit()
 
 
 def presentation_to_db(presentation: Presentation) -> Presentation_db:
