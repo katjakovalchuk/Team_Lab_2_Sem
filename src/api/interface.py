aebbc8050fb3c8d757211ebb2d37028797c208b9
radomyr <h.radomyr@proton.me>
2023-04-07 17:47:43 +0300

    temp: another try

--- src/api/interface.py
+++ src/api/interface.py
@@ -181,7 +181,8 @@ class PresentationAPI:
                     db.flush()
                     db.refresh(slide_db)
                 print(presentation.to_dict())
-            db.add(self.presentation)
+            db.flush()
+            db.refresh(self.presentation)
             db.commit()
         return {"slide_id": slide_id}
 
